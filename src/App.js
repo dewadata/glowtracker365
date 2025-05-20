8e490d6c1b1c5fa485e348f4ce7876b1e648e23c
dewadata <91824497+dewadata@users.noreply.github.com>
2025-05-20 10:30:22 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to GlowTracker365</h1>
+      <p>Your personal beauty and willpower tracker!</p>
+    </div>
+  );
+}
+
+export default App;
